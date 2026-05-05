2eab3aa3b7f9f839c075fbcc072bcbcfe6fb8e32
Ben M <109357901+ben-me@users.noreply.github.com>
2026-05-05 15:06:49 +0200

    init

---
+++ eslint.config.ts
@@ -0,0 +1,24 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu({
+  vue: true,
+  typescript: true,
+  formatters: true,
+  markdown: false,
+  stylistic: {
+    semi: true,
+    indent: 2,
+    quotes: "double",
+  },
+  plugins: {},
+}, {
+  rules: {
+    "perfectionist/sort-imports": ["error", { tsconfig: { rootDir: "." } }],
+    "no-console": "warn",
+    "ts/consistent-type-definitions": ["error", "type"],
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
+    }],
+  },
+});
---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+import postcssPresetEnv from "postcss-preset-env";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: false },
+  css: ["./app/assets/css/style.css"],
+  typescript: {
+    typeCheck: true,
+  },
+  postcss: {
+    plugins: {
+      "postcss-preset-env": postcssPresetEnv({
+        stage: 3,
+        features: {
+          "custom-media-queries": true,
+          "media-query-ranges": true,
+          "cascade-layers": true,
+          "nesting-rules": true,
+        },
+      }),
+    },
+  },
+});
