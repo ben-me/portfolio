5a8e717d9007fd4ee998b9f0a34deb757aa5daa6
Ben M <109357901+ben-me@users.noreply.github.com>
2026-05-18 12:12:14 +0200

    fix: fix postcss plugin and add blank pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-import postcssPresetEnv from "postcss-preset-env";
-
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
@@ -9,15 +7,14 @@ export default defineNuxtConfig({
   },
   postcss: {
     plugins: {
-      "postcss-preset-env": postcssPresetEnv({
+      "postcss-preset-env": {
         stage: 3,
         features: {
           "custom-media-queries": true,
           "media-query-ranges": true,
-          "cascade-layers": true,
           "nesting-rules": true,
         },
-      }),
+      },
     },
   },
   modules: ["@nuxt/image"],
